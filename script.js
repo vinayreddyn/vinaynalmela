b04b053747e78228f2196b03e3ff43d3cd980edc
vinayreddyn <vinayreddynnr@gmail.com>
2025-05-08 22:27:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add any JavaScript functionality you may need, such as smooth scrolling, animations, or interactive features.
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Portfolio loaded!");
+});
